2a0b19967c639bcebe1ed10ce487ebd58d7b84f1
Dmytro Barylo <bdpochta@gmail.com>
2019-03-10 18:41:01 +0200

    Add welcome screen

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Quiz/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quiz from "./components/Quiz.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Quiz
   }
-}
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Merriweather:400,700");
+@import url("https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css");
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background: #000;
+  color: #999;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Merriweather", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="quiz">
+    <div class="welcome">
+      <Picture
+        class="quiz-img"
+        url="https://media0.giphy.com/media/Bh3YfliwBZNwk/giphy.gif?cid=3640f6095c852266776c6f746fb2fc67"
+      />
+      <h1 class="quiz-heading">How Well Do You Know
+        <br>the Harry Potter Movies?
+      </h1>
+      <a href="#" class="quiz-button">Start Quiz</a>
+    </div>
+  </section>
+</template>
+
+<script>
+import Picture from "./Picture";
+export default {
+  name: "Quiz",
+  components: { Picture }
+};
+</script>
+
+<style>
+.quiz {
+  max-width: 500px;
+}
+.quiz-img {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+.quiz-heading {
+  margin: 30px 0;
+  line-height: 1.2;
+}
+.quiz-button {
+  display: inline-block;
+  padding: 10px 30px;
+  border: 1px solid #999;
+  color: #999;
+  text-decoration: none;
+  transition: background 0.5s, color 0.5s;
+}
+.quiz-button:hover {
+  background: #999999;
+  color: #000000;
+}
+</style>
