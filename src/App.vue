b4dd058fde567fb5b9b149083b93e7e9114a2ff0
Dmytro Barylo <bdpochta@gmail.com>
2019-03-11 22:55:53 +0200

    Finish app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <template>
-  <div id="app">
-    <Quiz/>
+  <div class="app">
+    <header class="app-header">
+      <span class="app-header-text">
+        Show on
+        <a href="https://github.com/dmtrbrl/hp-quiz">Github</a>
+      </span>
+      <h1 class="app-heading">
+        <svg
+          width="60"
+          height="60"
+          viewBox="0 0 60 60"
+          xmlns="http://www.w3.org/2000/svg"
+          class="app-heading-icon"
+        >
+          <path
+            d="M28.745 57.76a3.116 3.116 0 1 0 5.68-2.565L24.203 32.559l16.332 2.085a3.065 3.065 0 0 0 2.895-1.229 3.117 3.117 0 0 0 .347-3.126L31.315 2.252a3.115 3.115 0 0 0-5.692 2.531l10.213 22.98-16.37-2.09a3.085 3.085 0 0 0-2.901 1.236 3.117 3.117 0 0 0-.335 3.136L28.745 57.76z"
+          ></path>
+        </svg>
+        <span>Harry Potter</span>
+        <span>Movies Quiz</span>
+      </h1>
+      <span class="app-header-text">
+        Thanks
+        <a href="https://giphy.com/" target="_blank">GIPHY</a> for the gifs
+      </span>
+    </header>
+    <Quiz url="https://api.myjson.com/bins/19efxu"/>
   </div>
 </template>
 
@@ -29,13 +54,13 @@ html {
 
 body {
   height: 100%;
-  background: #000;
-  color: #999;
+  background: #020815;
+  color: #eee;
 }
 
-#app {
+.app {
   min-height: 100%;
-  padding: 15px;
+  padding: 95px 15px 30px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -43,5 +68,57 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+}
+.app-header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 95px;
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  font-size: 11px;
+}
+.app-header a {
+  color: #eee;
+  text-decoration: none;
+  border-bottom: 1px solid rgba(238, 238, 238, 0.5);
+}
+.app-heading {
+  position: absolute;
+  top: 50px;
+  left: 50%;
+  margin: 0;
+  padding-left: 20px;
+  font-size: 14px;
+  line-height: 15px;
+  transform: translateX(-50%);
+  cursor: default;
+  user-select: none;
+}
+@media (min-width: 600px) {
+  .app-header {
+    height: 90px;
+    padding: 30px 15px;
+    align-items: center;
+  }
+  .app-heading {
+    top: 30px;
+  }
+}
+.app-heading-icon {
+  position: absolute;
+  top: 1px;
+  left: -5px;
+  fill: #eee;
+  width: 26px;
+  height: 26px;
+}
+.app-heading span {
+  display: block;
+}
+.app-header-text {
+  opacity: 0.5;
 }
 </style>
